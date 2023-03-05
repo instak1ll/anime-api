2c0c2232dca69103fe842cfa4923f3da18389550
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-03-05 21:42:07 +0100

    En este commit se implementa una libreria 0.0.2 #2
    En este commit se implementa una libreria para dar efecto de 3d a las waifus gato

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Client, fetchRandom } from "nekos-best.js";
+import 'vanilla-tilt'
 
 const button = document.querySelector('#button')
 button.onclick = buscarInput
@@ -13,6 +14,12 @@ async function buscarInput() {
 
     const res = await fetch('https://nekos.best/api/v2/neko')
     const data = await res.json()
-    waifu.innerHTML += `<img src="${data.results[0].url}" width='228px' class='m-2'>`
-
+    const DIV = document.createElement('div')
+    DIV.innerHTML = `<img src="${data.results[0].url}" width='228px' class='m-2 border border-dark shadow-sm'>`
+    VanillaTilt.init(DIV, {
+        max: 25,
+        speed: 400
+    });
+    waifu.appendChild(DIV)
 }
+
