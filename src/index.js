431a9988ff64e89b2b26a8a536325b8ec6bc06dc
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-03-06 03:56:41 +0100

    Waifus de gato clicables 0.0.3 #3
    En esta nueva version podemos clicar en las imagenes de las waifus gatunas para
    ir a la imagen original y poder ver al author de la waifu gatuna

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Client, fetchRandom } from "nekos-best.js";
 import 'vanilla-tilt'
 
+
 const button = document.querySelector('#button')
 button.onclick = buscarInput
 const waifu = document.querySelector('#waifu')
@@ -15,11 +16,14 @@ async function buscarInput() {
     const res = await fetch('https://nekos.best/api/v2/neko')
     const data = await res.json()
     const DIV = document.createElement('div')
-    DIV.innerHTML = `<img src="${data.results[0].url}" width='228px' class='m-2 border border-dark shadow-sm'>`
+
+    DIV.innerHTML = `<a href="${data.results[0].source_url}" target="_blank"><img src="${data.results[0].url}" width='228px' class='m-2 border border-dark shadow-sm'></a>`
     VanillaTilt.init(DIV, {
         max: 25,
         speed: 400
     });
     waifu.appendChild(DIV)
 }
+
+
 
